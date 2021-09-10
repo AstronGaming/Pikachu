4fdbb71df5f3afd5ca613cc2cf9068d0fa2e4ec9
AstronGaming <yashudkl@Gmail.com>
2021-09-10 15:58:22 +0545

    commented

--- app.js
+++ app.js
@@ -9,21 +9,27 @@ app.use(express.static('./public'))
 app.use(express.urlencoded({extended:false}))
 
 
+//Setting up route for / request
 app.get('/',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'./public/index.html'))
 })
 
+//Setting up route for /contact request
 app.get('/contact',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'./public/contact.html'))
 })
 
+//Setting up route for /about request
 app.get('/about',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'./public/about.html'))
 })
 
+//Setting up route for /login request
 app.get('/login',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'./public/login.html'))
 })
+
+//Setting up route for /loginsucess request
 app.post('/loginsucess',(req,res)=>{
     const{name} = req.body
     if(name){
@@ -32,10 +38,12 @@ app.post('/loginsucess',(req,res)=>{
     res.status(401).send('please provide credentials')
 })
 
+//for all invalid request
 app.all('*',(req,res)=>{
     res.status(404).send('Resource not found')
 })
 
+//app listening at localhost 5000
 app.listen(5000,()=>{
     "App Is listening at port 5000"
 })
