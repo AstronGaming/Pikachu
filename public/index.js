38031d62f1b129f94bdb6d000006673648e12434
AstronGaming <yashudkl@Gmail.com>
2021-09-10 16:01:32 +0545

    comment

--- public/index.js
+++ public/index.js
@@ -1,10 +1,12 @@
-let a;
-let date;
-let time;
+let a; //declaring variable a
+let date;//declaring variable date
+let time; //declaring variable time
+
+//Using setInterval function
 setInterval(() => {
-a = new Date();
-date = a.toLocaleDateString();
-time = a.getHours() + ':' + a.getMinutes() + ':' + a.getSeconds();
-document.getElementById("time").innerHTML = 'Time ' + time;
-document.getElementById("date").innerHTML = 'Date ' +  date;
-}, 1000);
+a = new Date(); //Storing Date function in a variable which returns valur
+date = a.toLocaleDateString();//storing local date in date vairable
+time = a.getHours() + ':' + a.getMinutes() + ':' + a.getSeconds(); // Setting up time
+document.getElementById("time").innerHTML = 'Time ' + time; //Displaying time in innerhtml id time
+document.getElementById("date").innerHTML = 'Date ' +  date;//Displaying date in innerhtml using id date
+}, 1000); //interval of 1 second
